17b934e52889f8b593fa13b926516adc2887ed8e
CarlosClement12 <carlos.clement@iesdoctorbalmis.com>
2019-12-04 11:58:26 +0100

    Beautified code

--- ComandosMenus/MainWindow.xaml.cs
+++ ComandosMenus/MainWindow.xaml.cs
@@ -1,25 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace ComandosMenus
 {
-    /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
 
@@ -117,5 +104,6 @@ namespace ComandosMenus
         }
 
         #endregion COMMANDS
+
     }
 }
